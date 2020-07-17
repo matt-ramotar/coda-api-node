8714d728bdcc3e394183f5577a955b9fc3024a48
Matt Ramotar <matt.ramotar@jhu.edu>
2020-07-17 11:39:34 -0700

    Add headers key to config object applied to requests

--- package/function/getDoc.js
+++ package/function/getDoc.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getDoc(docId, token) {
   const uri = `https://coda.io/apis/v1beta1/docs/${docId}`;
-  const config = { Authorization: `Bearer ${token}` };
+  const config = { headers: { Authorization: `Bearer ${token}` } };
   const response = await axios.get(uri, config);
   return response.data;
 }
