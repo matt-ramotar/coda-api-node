37b2dacc26bc61e862da6f354984c5f0a246a0c7
Matt Ramotar <matt.ramotar@jhu.edu>
2020-07-17 13:05:38 -0700

    Add "listRows()" that returns rows for specified view

---
+++ package/function/listRows.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default async function listRows(docId, viewId, token) {
+  const uri = `https://coda.io/apis/v1beta1/docs/${docId}/views/${viewId}/rows`;
+  const config = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await axios.get(uri, config);
+  return response.data;
+}
