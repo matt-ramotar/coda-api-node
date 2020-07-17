1341ad3a19b553394735909f1064d8862a2de8aa
Matt Ramotar <matt.ramotar@jhu.edu>
2020-07-17 13:06:28 -0700

    Export "listRows"

--- package/function/index.js
+++ package/function/index.js
@@ -1,3 +1,4 @@
 export { default as getDoc } from './getDoc.js';
 export { default as getView } from './getView.js';
 export { default as listCols } from './listCols.js';
+export { default as listRows } from './ListRows.js';
