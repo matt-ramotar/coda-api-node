6f9db9060597c53630197b3299c020428199f164
Matt Ramotar <matt.ramotar@jhu.edu>
2020-07-17 13:25:13 -0700

    Export "getTable"

--- package/function/index.js
+++ package/function/index.js
@@ -2,3 +2,4 @@ export { default as getDoc } from './getDoc.js';
 export { default as getView } from './getView.js';
 export { default as listCols } from './listCols.js';
 export { default as listRows } from './ListRows.js';
+export { default as getTable } from './getTable.js';
