e26b3d0a0e49c9a5416882b09f90a0cfa37d46a8
Matt Ramotar <matt.ramotar@jhu.edu>
2020-07-17 11:32:35 -0700

    Update Coda class to include "getView" method

--- package/class/Coda.js
+++ package/class/Coda.js
@@ -8,4 +8,8 @@ export default class Coda {
   async getDoc(docId) {
     return await method.getDoc(docId, this.token);
   }
+
+  async getView(docId, viewId) {
+    return await method.getView(docId, viewId, this.token);
+  }
 }
